0af54bda2a00cdf8dcd2b8b4b36f7f32b16e20e6
jhamil545 <jhamil.apaza@upeu.edu.pe>
2023-11-30 08:33:41 -0500

    couchbases correc

--- src/main/java/com/cb/repo/CustomerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cb.repo;
-
-import com.cb.model.Customer;
-import org.springframework.data.couchbase.repository.CouchbaseRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepo extends CouchbaseRepository<Customer, Integer> {
-}
